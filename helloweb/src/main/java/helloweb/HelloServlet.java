351499fd74f4f924267cbecd5246092cc9358a68
Konahyeon <koo@inha.edu>
2024-12-17 13:24:49 +0900

    init commit

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,24 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.setContentType("text/html; charset=utf-8"); // 브라우저 헤더 부분에 줌
+    	PrintWriter out = response.getWriter(); // 빈 개행을 줌 (이 다음은 body)
+    	out.print("<h1>Hello World</h1>");
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
