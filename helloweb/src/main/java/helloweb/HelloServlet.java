6b90a13a9dac8184a1e64d39d052bef1868c571c
Konahyeon <koo@inha.edu>
2024-12-17 15:22:40 +0900

    servlet programming #1

--- helloweb/src/main/java/helloweb/HelloServlet.java
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -12,9 +12,14 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String id = request.getParameter("id");
+    	String no = request.getParameter("no");
+    	
+    	System.out.println(id + ":" + no);
+    	
     	response.setContentType("text/html; charset=utf-8"); // 브라우저 헤더 부분에 줌
     	PrintWriter out = response.getWriter(); // 빈 개행을 줌 (이 다음은 body)
-    	out.print("<h1>Hello World</h1>");
+    	out.print("<h1>Hello " + id + "</h1>");
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- helloweb/src/main/webapp/tags.jsp
+++ helloweb/src/main/webapp/tags.jsp
@@ -13,5 +13,39 @@
 	<h4>안녕하세요~</h4>
 	<h5>안녕하세요~</h5>
 	<h6>안녕하세요~</h6>
+	
+	<table border="1" cellspacing="0" cellpadding="10">
+		<tr>
+			<th>글번호</th>
+			<th>글제목</th>
+			<th>작성자</th>
+		</tr>
+		
+		<tr>
+			<th>1</th>
+			<th>안녕</th>
+			<th>둘리</th>
+		</tr>
+		
+		<tr>
+			<th>2</th>
+			<th>방가</th>
+			<th>또치</th>
+		</tr>
+	</table>
+	
+	<img src="http://localhost:8080/helloweb/images/loopy.jpeg" style="witdh:100px">
+	<img src="/helloweb/images/loopy.jpeg" style="witdh:100px">
+	<img src="images/loopy.jpeg" style="witdh:100px">
+	
+	<p>
+		문장입니다
+		문장입니다
+		문장입니다
+	</p>
+	
+	<a href="/helloweb/hello?id=gonow&no=24">hello로 가기</a>
+	<br>
+	<a href="/helloweb/form.jsp">form으로 가기</a>
 </body>
 </html>
